b9e46078be5e989a973dda8e7721c70ef78a3a0b
Alex Chistyakov <alexclear@gmail.com>
2015-09-18 17:08:54 +0300

    Add multi-environment capabilities

--- templates/hbase/thrift.rb
+++ templates/hbase/thrift.rb
@@ -1,12 +1,12 @@
-Eye.application 'thrift-server' do
+Eye.application 'hbase-thrift-server-{{ env_name }}' do
   working_dir '/etc/eye'
-  stdall '/var/log/eye/thrift-server-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/hbase-thrift-server-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :thrift do
-    pid_file '/var/hbase/hbase_thrift.pid'
-    start_command 'sudo -u hadoop /opt/hbase/bin/hbase thrift start'
+  process :hbase_thrift_{{ env_name }} do
+    pid_file '{{ hbase_var_prefix }}/hbase_thrift.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hbase_distr_prefix }}/bin/hbase thrift start'
 
     daemonize true
     start_timeout 10.seconds
